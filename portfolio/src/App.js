fd47162ce7cb1004831b7b697f0bad7f4018f9ff
loadywiseman@gmail.com <joshy12>
2023-07-28 17:11:54 +0300

    end in desinin the system

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Header from './components/Header';
 import CardEditor from './components/CardEditor';
 import CardPreview from './components/CardPreview';
+import CardSettings from './components/CardSettings';
 
 const App = () => {
   const [cardTitle, setCardTitle] = useState('');
@@ -19,15 +20,29 @@ const App = () => {
     <div className="app">
       <Header />
       <div className="container">
-        <CardEditor
-          cardTitle={cardTitle}
-          cardMessage={cardMessage}
-          onTitleChange={handleTitleChange}
-          onMessageChange={handleMessageChange}
-        />
-        <CardPreview cardTitle={cardTitle} cardMessage={cardMessage} />
+        {/* Card Editor Section */}
+        <section id="cardEditor">
+          <h2>Card Editor</h2>
+          <CardEditor
+            cardTitle={cardTitle}
+            cardMessage={cardMessage}
+            onTitleChange={handleTitleChange}
+            onMessageChange={handleMessageChange}
+          />
+        </section>
+
+        {/* Card Preview Section */}
+        <section id="cardPreview">
+          <h2>Card Preview</h2>
+          <CardPreview cardTitle={cardTitle} cardMessage={cardMessage} />
+        </section>
+
+        {/* Card Settings Section */}
+        <section id="cardSettings">
+          <h2>Card Settings</h2>
+          <CardSettings />
+        </section>
       </div>
-      {/* Add CardSettings component here if desired */}
     </div>
   );
 };
--- portfolio/src/components/Header.js
+++ portfolio/src/components/Header.js
@@ -1,10 +1,19 @@
 import React from 'react';
-
+import './Header.css';
 const Header = () => {
   return (
     <header>
       <h1>Wedding Card Creator</h1>
-      {/* Add any other header content here */}
+      {/* Add any other header content here */
+      
+      <nav>
+        <ul>
+          <li><a href="#cardEditor">Card Editor</a></li>
+          <li><a href="#cardPreview">Card Preview</a></li>
+          <li><a href="#cardSettings">Card Settings</a></li>
+        </ul>
+      </nav>
+      }
     </header>
   );
 };
